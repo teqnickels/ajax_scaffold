6e2f9e7d6f35634fbb110b3deae09023d8b4200b
Bonnie Schulkin <bonnie@learnersguild.com>
2017-10-30 11:50:27 -0700

    Implement function that grabs data via AJAX and loads into the 'answer' div.

--- magic8ball/public/magic8.js
+++ magic8ball/public/magic8.js
@@ -1,1 +1,8 @@
-console.log('I am here')
+const get8response = () => {
+  fetch('/magic8response')
+    .then(result => result.text())
+    .then((text) => {
+      document.getElementById('answer').textContent = text
+    })
+    .catch(err => console.log(err))
+}
