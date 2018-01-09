23eea7f02fe97bb03c2e9eaf4320a0618a6e6fe9
Bonnie Schulkin <bonnie@learnersguild.com>
2018-01-09 09:47:58 -0800

    Revert to pristine, pre-solve state

--- magic8ball/public/magic8.js
+++ magic8ball/public/magic8.js
@@ -1,16 +1,1 @@
-const get8response = () => {
-  console.log('got here')
-  fetch('/magic8response')
-    .then(result => result.text())
-    .then((text) => {
-      document.getElementById('answer').textContent = text
-    })
-    .catch(err => console.log(err))
-}
-
-document.getElementById('question-form')
-  .addEventListener('submit', (evt) => {
-    evt.preventDefault()
-    console.log('hiya')
-    get8response()
-  })
+// write ajax code here
